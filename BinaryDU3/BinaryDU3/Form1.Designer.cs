9947ce781dfff5f42e41d86ba852011a900031d8
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-04-02 18:47:22 +0200

    Add files via upload

---
+++ BinaryDU3/BinaryDU3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BinaryDU3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(542, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(246, 420);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(265, 13);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(271, 420);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "<-- Neopravený\r\n\r\nOpravený -->";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BinaryDU3/BinaryDU3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BinaryDU3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("texty.dat",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs, Encoding.GetEncoding("windows-1250"));
+            bw.Write("Toto je první věta. Tohle druhá.");
+            bw.Write("Tady je druhý řádek. To je ale pěkné.");
+            bw.Write("A tady je řádek poslední. Třetí.");
+            BinaryReader br = new BinaryReader(fs, Encoding.GetEncoding("windows-1250"));
+            br.BaseStream.Position = 0;
+            while(br.BaseStream.Position < br.BaseStream.Length)
+            {
+                listBox1.Items.Add(br.ReadString());
+            }
+            fs.Close();
+            fs = new FileStream("texty.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            bw = new BinaryWriter(fs, Encoding.GetEncoding("windows-1250"));
+            foreach(string str in listBox1.Items)
+                bw.Write(str.Replace('.','!'));
+            br = new BinaryReader(fs, Encoding.GetEncoding("windows-1250"));
+            br.BaseStream.Position = 0;
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                listBox2.Items.Add(br.ReadString());
+            }
+            fs.Close();
+        }
+    }
+}
